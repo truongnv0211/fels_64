3c3ac25fde3c2487c0df7ea63e3f88021d336dc1
truongnv0211 <nguyen.van.truong@framgia.com>
2015-06-24 13:41:37 +0700

    log in - log out

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150622101412) do
+ActiveRecord::Schema.define(version: 20150624030754) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
   end
 
   create_table "lesson_words", force: :cascade do |t|
