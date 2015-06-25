bc977f5083c0406ebeccbadce63b7f01c98aca62
truongnv0211 <nguyen.van.truong@framgia.com>
2015-06-25 10:59:35 +0700

    Admin dashboard

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,14 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by email: params[:session][:email].downcase
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      flash.now[:success] = t :alert_login_success
-      redirect_to user
+      if user.admin?
+        log_in user
+        redirect_to admin_root_path
+      else
+        log_in user
+        flash[:success] = t :alert_login_success
+        redirect_to user
+      end
     else
       flash.now[:danger] = t :alert_login_invalid
       render "new"
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150624030754) do
+ActiveRecord::Schema.define(version: 20150625031439) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "title"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.text     "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "lesson_words", force: :cascade do |t|
@@ -46,9 +45,9 @@ ActiveRecord::Schema.define(version: 20150624030754) do
     t.string   "username"
     t.string   "email"
     t.string   "password_digest"
-    t.boolean  "admin"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.boolean  "admin",           default: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
   end
 
   create_table "words", force: :cascade do |t|
