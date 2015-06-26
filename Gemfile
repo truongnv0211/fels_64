791d6316b358a4cddbeb6ec5ba55d90123677943
Vu Minh Duc <vu.minh.duc@framgia.com>
2015-06-26 16:22:30 +0700

    List all users and delete user

--- Gemfile
+++ Gemfile
@@ -14,6 +14,8 @@ gem "rails_config", "~> 0.4.2"
 gem "carrierwave", "0.10.0"
 gem "mini_magick", "3.8.0"
 gem "fog", "~> 1.31.0"
+gem "will_paginate", "~> 3.0.7"
+gem "bootstrap-will_paginate", "~> 0.0.10"
 
 group :development, :test do
   gem "sqlite3"
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: [:destroy]
+
+  def index
+    @users = User.paginate page: params[:page], per_page: Settings.users_per_index
+  end
 
   def show
     @user = User.find params[:id]
@@ -35,6 +40,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = t :user_delete_message
+    redirect_to users_url
+  end
+
   private
   def user_params
     params.require(:user).permit :username, :email, :password,
@@ -42,7 +53,11 @@ class UsersController < ApplicationController
   end
 
   def correct_user
-    user = User.find params[:id]
-    redirect_to root_path unless current_user? user
+    @user = User.find params[:id]
+    redirect_to root_path unless current_user? @user
+  end
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?
   end
 end
