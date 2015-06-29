3246e71bcd26178e2a4fdbc344292f2c156f809c
truongnv0211 <nguyen.van.truong@framgia.com>
2015-06-29 11:38:02 +0700

    Category front end - new seed.rb

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,4 +2,5 @@ class Lesson < ActiveRecord::Base
   has_many :words, through: :lesson_words
   has_many :lesson_words
   belongs_to :user
+  belongs_to :category
 end
--- db/schema.rb
+++ db/schema.rb
@@ -39,8 +39,9 @@ ActiveRecord::Schema.define(version: 20150629082450) do
   create_table "lessons", force: :cascade do |t|
     t.string   "title"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "relationships", force: :cascade do |t|
